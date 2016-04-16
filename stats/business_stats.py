7630423960ab1c17169886618ae6c1105cc1d9f1
explorerwjy <explorerwjy@gmail.com>
2016-04-16 21:57:36 +0000

    update business_stats.py

--- stats/business_stats.py
+++ stats/business_stats.py
@@ -14,22 +14,49 @@ import json
 def group_by_city(data):
     count_by_city = data.map(lambda x: (x["city"],1))
     pairs1 = count_by_city.reduceByKey(lambda x,y:x+y)
-    counts = pairs1.sortByKey(True).collect()
+    pairs2 = pairs1.map(lambda x: (x[1],x[0])).sortByKey(False).map(lambda x: (x[1].encode('utf-8').strip(),x[0]))
+    top_10_city = pairs2.map(lambda x: x[0].encode('utf-8').strip()).take(10)
+    counts = pairs2.collect()
 
-    with open("business_count_by_city",'w') as fout:
-        for (city,counts) in counts:
-            fout.write("{}\t{}\n".format(data,count))
+    with open("business_count_by_city.txt",'w') as fout:
+        for (city,count) in counts:
+            fout.write("{}\t{}\n".format(city,count))
+    return top_10_city
 
+def group_by_city_cat(data,city):
+    print data.take(10)
+    print data.map(lambda x:x["city"]).take(10)
+    city_data = data.filter(lambda x: x["city"].encode('utf-8').strip() == city)
+    print city_data.map(lambda x: x["city"]).take(10)
+    count_by_cat = city_data.map(lambda x: (x["categories"],1))
+    pairs1 = count_by_cat.reduceByKey(lambda x,y : x+y)
+    pairs2 = pairs1.map(lambda x: (x[1],x[0])).sortByKey(False).map(lambda x: (x[1].encode('utf-8').strip(),x[0]))
+    counts = pairs2.collect()
+    
+    file_path = 'city_'+'_'.join(city.split())+'txt'
+    with open(file_path,'w') as fout:
+        for (cat,count) in counts:
+            fout.write("{}\t{}\n".format(cat,count))
 
+    return
+def stats_on_city(data,n):
+    handle = open("business_count_by_city.txt",'r')
+    cities = []
+    for i in range(n):
+        cities.append(handle.readline().strip().split('\t')[0])
+    print cities
+    for i in range(n):
+        group_by_city_cat(data,cities[i])
+    return
 
 if __name__ == "__main__":
 
     infile =  's3://anly502-yelp/yelp_academic_dataset_business.json'
+    #infile = 'https://s3.amazonaws.com/anly502-yelp/yelp_academic_dataset_business.json'
     sc     = SparkContext( appName="Business_stats" )
     lines = sc.textFile(infile)
     data = lines.map(lambda x: json.loads(x))
-    print data.take(10)
-
-
+    #group_by_city(data)
+    stats_on_city(data,1) 
 
     sc.stop()
