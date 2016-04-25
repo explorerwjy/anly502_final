7ee0d31dfaf9a5c634a0679e5ccf17fe2f0c39c6
explorerwjy <explorerwjy@gmail.com>
2016-04-25 21:48:45 +0000

    update

--- data_preperation/filter_data.py
+++ data_preperation/filter_data.py
@@ -7,25 +7,29 @@ import json
 
 # get the list of business that are top [N] [category] business from [city] with most reviews
 def get_business_list(num,category,city,business_data):
-	cat_data = business_data.filter(lambda x: (category in x["categories"]) and (city in x["city"]))
+    cat_data = business_data.filter(lambda x: (category in x["categories"]) and (city in x["city"]))
     bus = cat_data.map(lambda x: (x,x['review_count']))
     counts = bus.map(lambda x : (x[1],x[0])).sortByKey(False).map(lambda x: (x[1],x[0]))
-    counts = counts.map(lambda x: (x[0]["business_id"],x[0]["name"],x[0]["stars"],x[1])).take(n)
+    counts = counts.map(lambda x: (x[0]["business_id"],x[0]["name"],x[0]["stars"],x[1])).take(num)
     city_name = "_".join(city.split())
-    file_name = "best_"+str(n)+"_"+category+"_in_"+city_name+".txt"
+    file_name = "best_"+str(num)+"_"+category+"_in_"+city_name+".txt"
     with open(file_name,'w') as fout:
-        for (bus,name,city,,stars,count) in counts:
-            fout.write("{}\t{}\t{}\t{}\n".format(bus,name,stars,count))
+        for (bus,name,stars,count) in counts:
+            fout.write("{}\t{}\t{}\t{}\n".format(bus.encode('utf-8'),name.encode('utf-8'),stars,count))
     return file_name
 
 # filter and save data we need for training data set
 def make_name(file_name,extra):
 	a,b = file_name.split('.')
 	name = a+'_'+extra+".json"
+        return name
 def get_data_according_to_list_file(bus_list_file,business_data,review_data,tip_data,user_data):
 	#get business list according to list file
 	handle = open(bus_list_file,'r')
-	bus_list = [id_ for id_,name,start,count in handle.readline().split('\t')]
+        bus_list = []
+        for l in handle:
+            id_,name,start,count = l.split('\t')
+            bus_list.append(id_)
 	print bus_list
 	#filter business 
 	filtered_business_data = business_data.filter(lambda x:x["business_id"] in bus_list)
@@ -77,11 +81,11 @@ def get_words(review_data,num_dic,num_sample):
 	distinct_positive = distinct_positive.take(num_dic)
 	distinct_negetive = distinct_negetive.take(num_dic)
 	with open("good_words.txt",'w') as fout:
-        for word in distinct_positive:
-            fout.write("{}\n".format(k,v))
-    with open("bad_words.txt",'w') as fout:
-        for word in distinct_negetive:
-            fout.write("{}\n".format(k,v))
+            for word in distinct_positive:
+                fout.write("{}\n".format(k,v))
+        with open("bad_words.txt",'w') as fout:
+            for word in distinct_negetive:
+                fout.write("{}\n".format(k,v))
 	
 
 #construct training data and output csv file
@@ -126,8 +130,8 @@ def group_review_by_time(x):
 			make_record(review,time_diction[review[0]])
 	time_diction = time_diction.items()
 	time_data = [] #year_month,sum_of_last_three_month([#total,#5star,#4star,#3star,#2star,#1star])
-	for i in xrange(3:len(time_diction)):
-		last_three = [[time_diction[i-1][1],[time_diction[i-2][1],[time_diction[i-3][1]]
+	for i in xrange(3,len(time_diction)):
+		last_three = [[time_diction[i-1][1],[time_diction[i-2][1],[time_diction[i-3][1]]]]]
 		record = (time_diction[i][0],combine_record(last_three))
 		time_data.append(record)
 
@@ -159,6 +163,7 @@ def construct_training_data_v1(business_file,user_file,review_file,tip_file):
 
 
 if __name__ == "__main__":
+    
 
     business_file =  's3://anly502-yelp/yelp_academic_dataset_business.json'
     user_file =  's3://anly502-yelp/yelp_academic_dataset_user.json'
@@ -174,8 +179,9 @@ if __name__ == "__main__":
     tip_data = sc.textFile(tip_file).map(lambda x: json.loads(x))
 
     # Procedures starts
-    fileanme = get_business_list(100,"Restaurants","Las Vegas",business_data)
-    #get_data_according_to_list_file(fileanme,business_data,review_data,tip_data,user_data)
+    #fileanme = get_business_list(1000,"Restaurants","Las Vegas",business_data)
+    filename = "best_1000_Restaurants_in_Las_Vegas.txt"
+    get_data_according_to_list_file(filename,business_data,review_data,tip_data,user_data)
     #get_words(review_data,500,5000000)
 
 
