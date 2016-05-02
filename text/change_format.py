df0cca926fb45e1b4d35248db6a27e38e0e81cd3
explorerwjy <explorerwjy@gmail.com>
2016-05-02 03:48:06 +0000

    commit

--- text/change_format.py
+++ text/change_format.py
@@ -1,4 +1,50 @@
+#!/usr/bin/python
+
+from random import shuffle
 from sys import argv
+
+def make_v4(file_name):
+    hand = open(file_name,'r')
+    out_file = open(file_name+'_v4','w')
+    classes = {}
+    total = 0
+    zero,one = [],[]
+    for l in hand:
+        l = l.strip()
+        label = l.split()[0]
+        value = " ".join(l.split()[1:])
+        #value = '1:' + value[0] + ' 2:' + value[1] + ' 3:' + value[2]
+        if label == '1':
+            one.append((label,value))
+        elif label == '0':
+            zero.append((label,value))
+    length = len(zero)
+    shuffle(one)
+    one = one[0:length]
+    print len(one),len(zero)
+    for k,v in one :
+        out_file.write(k+" "+v+'\n')
+    for k,v in zero:
+        out_file.write(k+" "+v+"\n")
+    
+
+
+
+def count_percent(file_name):
+    hand = open(file_name,'r')
+    out_file = open('format_'+file_name,'w')
+    classes = {}
+    total = 0
+    for l in hand:
+        l = l.strip()
+        label = l.split()[0]
+        if label not in classes:
+            classes[label] = 1
+        else:
+            classes[label] += 1
+        total += 1
+    for k,v in classes.items():
+        print k+" take a percent of:",float(v)/total
 
 def label_value(file_name):
     hand = open(file_name,'r')
@@ -25,8 +71,10 @@ def multi_bin(file_name):
         out_file.write(label+' '+value+'\n')
 
 def main():
+    #count_percent(argv[1])
     #label_value(argv[1])
-    multi_bin(argv[1])
+    #multi_bin(argv[1])
+    make_v4(argv[1])
 
 if __name__=="__main__":
     main()
--- text/trainModel.py
+++ text/trainModel.py
@@ -35,45 +35,53 @@ def ramdonForest(sc):
 
 def Boosting(sc):
     # Load and parse the data file.
-    data = MLUtils.loadLibSVMFile(sc, 's3://anly502-yelp/bin_regression_review_text_v3')
+    data = MLUtils.loadLibSVMFile(sc, 's3://anly502-yelp/bin_regression_review_text_v3_v4')
     # Split the data into training and test sets (30% held out for testing)
     (trainingData, testData) = data.randomSplit([0.7, 0.3])
 
     # Train a GradientBoostedTrees model.
     #  Notes: (a) Empty categoricalFeaturesInfo indicates all features are continuous.
     #         (b) Use more iterations in practice.
-    model = GradientBoostedTrees.trainClassifier(trainingData,
-                                                         categoricalFeaturesInfo={}, numIterations=3)
+    model = GradientBoostedTrees.trainClassifier(trainingData,learningRate = 0.05,maxDepth=5,maxBins=32,categoricalFeaturesInfo={}, numIterations=300)
 
     # Evaluate model on test instances and compute test error
     predictions = model.predict(testData.map(lambda x: x.features))
     labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+    haha = labelsAndPredictions.collect()
+    with open('predictions.txt','w') as fout:
+        for v,p in haha:
+            fout.write(str(v)+" "+str(p)+'\n')
     testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testData.count())
     print('Test Error = ' + str(testErr))
     print('Learned classification GBT model:')
     print(model.toDebugString())
 
     # Save and load model
-    model.save(sc, "target/tmp/myGradientBoostingClassificationModel")
-    sameModel = GradientBoostedTreesModel.load(sc,"target/tmp/myGradientBoostingClassificationModel")
+    #model.save(sc, "target/tmp/myGradientBoostingClassificationModel")
+    #sameModel = GradientBoostedTreesModel.load(sc,"target/tmp/myGradientBoostingClassificationModel")
 
 def parsePoint(line):
     values = [float(x) for x in line.split(' ')]
     return LabeledPoint(values[0], values[1:])
 
 def SVM(sc):
+    #data = sc.textFile("s3://anly502-yelp/bin_regression_review_text_v3")
+    #parsedData = data.map(parsePoint)
 
-    data = sc.textFile("s3://anly502-yelp/")
-    
-    parsedData = data.map(parsePoint)
-
+    data = MLUtils.loadLibSVMFile(sc, 's3://anly502-yelp/bin_regression_review_text_v3')
+    # Split the data into training and test sets (30% held out for testing)
+    (trainingData, testData) = data.randomSplit([0.7, 0.3])
     # Build the model
-    model = SVMWithSGD.train(parsedData, iterations=100)
+    model = SVMWithSGD.train(trainingData, iterations=100)
 
-    # Evaluating the model on training data
-    labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
-    trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
-    print("Training Error = " + str(trainErr))
+    # Evaluating the model on test data
+    predictions = model.predict(testData.map(lambda x: x.features))
+    #labelsAndPreds = parsedData.map(lambda p: (p.label, model.predict(p.features)))
+    labelsAndPredictions = testData.map(lambda lp: lp.label).zip(predictions)
+    testErr = labelsAndPredictions.filter(lambda (v, p): v != p).count() / float(testData.count())
+    print('Test Error = ' + str(testErr))
+    #trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
+    #print("Training Error = " + str(trainErr))
 
     # Save and load model
     model.save(sc, "myModelPath")
@@ -83,7 +91,8 @@ def main():
     sc     = SparkContext( appName="Train model" )
     
     #randomForest(sc)
-    Boosting(sc) 
+    Boosting(sc)
+    #SVM(sc)
 
 if __name__=="__main__":
     main()
